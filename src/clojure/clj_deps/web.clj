7e319ca2b4766adf6fa07a33af248e77ac070e07
rod <rod@pu-gh.com>
2013-05-22 22:51:43 +0100

    added simple html file

--- src/clojure/clj_deps/web.clj
+++ src/clojure/clj_deps/web.clj
@@ -3,34 +3,35 @@
   (:require [clj-deps.project :refer [description->project]]
             [clj-deps.maven :refer [project->versions]]
             [clj-deps.checker :refer [project->status]]
+            [net.cgrand.enlive-html :refer :all]
             [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
             [ring.util.response :refer [file-response]]
-            [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.stacktrace :refer [wrap-stacktrace]]))
 
-(defn req->name [{:keys [params]}]
-  (format "%s/%s"
-          (:repo params)
-          (:user params)))
+(defn req->description [{:keys [params]}]
+  {:source :github
+   :name (format "%s/%s"
+                 (:repo params)
+                 (:user params))})
 
 (defn png-for [stability req]
-  (let [description {:source :github
-                     :name (req->name req)}
-        status (-> description
+  (let [status (-> (req->description req)
                    (description->project)
                    (project->versions)
                    (project->status))
-        file (if (empty? (stability status))
-               "uptodate"
-               "outdated")]
+        result (if (empty? (stability status))
+                 "uptodate"
+                 "outdated")]
     (file-response
-      (format "resources/images/%s.png"
-              file))))
+      (format "resources/images/%s.png" result))))
+
+(deftemplate www-index "index.html"
+  [req])
 
 (defroutes all-routes
-  (GET "/" [] "Hello, World")
+  (GET "/" [] www-index)
   (GET "/:repo/:user/stable.png" [] (partial png-for :stable))
   (GET "/:repo/:user/unstable.png" [] (partial png-for :unstable))
   (route/resources "/assets")
@@ -38,7 +39,6 @@
 
 (def app
   (-> #'all-routes
-      (wrap-reload)
       (wrap-stacktrace)
       (handler/site)))
 
