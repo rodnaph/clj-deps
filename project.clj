8e587b4d4662c5311fe3c1b62de632419bf607a0
rod <rod@pu-gh.com>
2013-06-02 18:15:09 +0100

    updated router to 0.3.0

--- project.clj
+++ project.clj
@@ -7,12 +7,11 @@
                  [clj-logging-config "1.9.10"]
                  [boxuk/versions "0.6.1"]
                  [confo "0.5.1"]
-                 [router "0.2.0"]
+                 [router "0.3.0"]
                  [compojure "1.1.5"]
                  [ring/ring-jetty-adapter "1.1.8"]
                  [ring/ring-devel "1.1.8"]
-                 [enlive "1.1.1"]
-                 [router "0.2.0"]]
+                 [enlive "1.1.1"]]
   :source-paths ["dev" "src/clojure" "src/html"]
   :min-lein-version "2.0.0"
   :main clj-deps.core)
